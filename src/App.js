2c909a72d31f4a683497d23fa3f7c1df16b48864
Thomas <schmiddla@gmail.com>
2022-04-02 00:52:26 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Nav from './views/Nav';
+import Footer from './views/Footer';
+import Profile from './views/Profile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <main className="container-fluid p-0 m-0">
+        <Routes>
+          <Route path={`/`} element={<Profile />} />
+          {/* <Route path={`/search`} element={<SearchResults />} />
+          <Route path={`/recipe/:id`} element={<Recipe />} /> */}
+        </Routes>
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/views/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './css/footer.css';
+
+export default function Footer() {
+  return (
+    // <!-- Footer -->
+    <footer>Footer</footer>
+  );
+}
